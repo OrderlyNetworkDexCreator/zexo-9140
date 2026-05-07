e9b35eff0f84052eeb1fe58bd0dac265b604a9e1
Mario Reder <mario.reder@pm.me>
2026-05-07 20:59:34 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://app.zexo.io",
+  "VITE_SEO_SITE_URL": "https://zexo-sigma.vercel.app",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
